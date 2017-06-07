1b42f095d7c1b37a5e1fb96ea6cb8d23e4fb0087
Nick Pope <pope410211@gmail.com>
2017-06-06 22:18:53 -0400

    part 2 of Angular 2 Tour of Heroes (ToH) tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,98 @@
 import { Component } from '@angular/core';
 
 export class Hero {
-	id: number:
+	id: number;
 	name: string;
 }
+
+const HEROES: Hero[] = [
+	{id: 1, name: 'Nezzerand Silverlight'},
+	{id: 2, name: 'Lady Idril'},
+	{id: 3, name: 'Drizzt Do\' Urden'},
+	{id: 4, name: 'Bruenor Battlehammer'},
+	{id: 5, name: 'Commander Shephard'},
+	{id: 6, name: 'Dr. Liara T\'Soni'},
+	{id: 7, name: 'Gandalf the White'},
+	{id: 8, name: 'Samwise Gamgee'},
+	{id: 9, name: 'Peregrin Took'},
+	{id: 10, name: 'Meriadoc Brandybuck'}
+];
 
 @Component({
 	selector: 'my-app',
 	template: `
 		<h1>{{title}}</h1>
-		<h2>{{hero.name}} details!</h2>
-		<div><label>id: </label>{{hero.id}}</div>
-		<div>
-			<label>Name</label>
-			<input [(ngModel)]="hero.name" placeholder="Heroes Name" />
+		<h2>My Heroes</h2>
+		<ul class="heroes">
+			<li [class.selected]="hero === selectedHero" *ngFor="let hero of heroes" (click)="onSelect(hero)">
+				<span class="badge">{{hero.id}}</span> {{hero.name}}
+			</li>
+		</ul>
+		<div *ngIf="selectedHero">
+			<h2>{{selectedHero.name}} details!</h2>
+			<div><label>id: </label>{{selectedHero.id}}</div>
+			<div>
+				<label>Name</label>
+				<input [(ngModel)]="selectedHero.name" placeholder="Heroes Name" />
+			</div>
 		</div>
-		`
-
+		`,
+	styles: [`
+		.selected {
+			background-color: #CFD8DC !important;
+			color: white;
+		}
+		.heroes {
+			margin: 0 0 2em 0;
+			list-style-type: none;
+			padding: 0;
+			width: 15em;
+		}
+		.heroes li {
+			cursor: pointer;
+			position: relative;
+			left: 0;
+			background-color: #EEE;
+			margin: .5em;
+			padding: .3em 0;
+			height: 1.6em;
+			border-radius: 4px;
+		}
+		.heroes li.selected:hover {
+			background-color: #BBD8DC !important;
+			color: white;
+		}
+		.heroes li:hover {
+			color: #607D8B;
+			background-color: #DDD;
+			left: .1em;
+		}
+		.heroes .text {
+			position: relative;
+			top: -3px;
+		}
+		.heroes .badge {
+			display: inline-block;
+			font-size: small;
+			color: white;
+			padding: 0.8em 0.7em 0 0.7em;
+			background-color: #607D8B;
+			line-height: 1em;
+			position: relative;
+			left: -1px;
+			top: -4px;
+			height: 1.8em;
+			margin-right: .8em;
+			border-radius: 4px 0 0 4px;
+		}
+	`]
 })
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero: Hero {
-		id: 1,
-		name: 'Windstorm'
+	heroes = HEROES;
+	selectedHero: Hero;
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
 	}
 }
