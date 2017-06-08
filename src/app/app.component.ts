5f87f4d10d31192219e40b88ea250cd9969db468
Nick Pope <pope410211@gmail.com>
2017-06-07 21:54:12 -0400

    added hero-detail.component.ts & hero.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-	id: number;
-	name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
 	{id: 1, name: 'Nezzerand Silverlight'},
@@ -28,14 +24,6 @@ const HEROES: Hero[] = [
 				<span class="badge">{{hero.id}}</span> {{hero.name}}
 			</li>
 		</ul>
-		<div *ngIf="selectedHero">
-			<h2>{{selectedHero.name}} details!</h2>
-			<div><label>id: </label>{{selectedHero.id}}</div>
-			<div>
-				<label>Name</label>
-				<input [(ngModel)]="selectedHero.name" placeholder="Heroes Name" />
-			</div>
-		</div>
 		`,
 	styles: [`
 		.selected {
